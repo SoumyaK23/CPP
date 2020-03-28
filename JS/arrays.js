7a56dd3e2926d67e79f7d18585e5fdf7dbabef6c
SoumyaK23 <soumya.krishnamurthy@gmail.com>
2020-03-28 22:54:52 +0000

    Add files via upload

---
+++ JS/arrays.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    nums.sort()
+    let max_num = Math.max.apply(null,nums);
+    let remove_max = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== max_num) {
+            remove_max.push(nums[i]);
+        }
+    }
+    let second_max = Math.max.apply(null, remove_max);
+    return second_max;
+
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
